d64706f5ccd4398f36f56dca9e59912dee227405
Lonxyboi <drewthemachine22@gmail.com>
2022-08-19 10:57:58 -0400

    finished user controller just need to do through controller and routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -30,8 +30,34 @@ const userController = {
             console.log(err);
             res.sendStatus(400);
         });
-    }
+    },
 
+    //create users
+    createUser({ body }, res){
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+    //update user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No Users Found :(' })
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    //delete the user :)
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
     
 };
 
